b063d1670cd233c1f66856116890e5f0a1070ca2
maxim <dkzt.kontakt@gmail.com>
2021-02-20 18:26:38 +0100

    Add lombok and customer

---
+++ src/main/java/com/tutorial/module/Customer.java
@@ -0,0 +1,18 @@
+package com.tutorial.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author maximilian lamm brain.free.kontakt@gmail.com
+ * @project CoustomerOrderTutorial
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    String name;
+
+}
