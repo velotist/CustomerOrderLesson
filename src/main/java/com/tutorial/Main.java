2bc3a42d85a737396a61c48a73d8325bfb1e7cc7
maxim <dkzt.kontakt@gmail.com>
2021-02-20 18:17:29 +0100

    Initial commit

---
+++ src/main/java/com/tutorial/Main.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+/**
+ * @author maximilian lamm brain.free.kontakt@gmail.com
+ * @project CoustomerOrderTutorial
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
