d30bfbcc9a017c7bb99dbba9084e6ec97dca2964
maxim <dkzt.kontakt@gmail.com>
2021-02-20 18:47:52 +0100

    Add writeFile method

--- src/main/java/com/tutorial/Main.java
+++ src/main/java/com/tutorial/Main.java
@@ -1,11 +1,24 @@
 package com.tutorial;
 
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author maximilian lamm brain.free.kontakt@gmail.com
  * @project CoustomerOrderTutorial
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static void main(String[] args) throws IOException {
+        wirteFile();
+    }
+
+    private static void wirteFile() throws IOException {
+        System.out.println("hier wird eine Datei gelesen" + getPath().toString() + " order.text");
+    }
+
+    private static Path getPath(){
+        return Paths.get("/resources/");
     }
 }
