256b88ee14ff9f264e6ad692f05b1a3230590af8
velotist <velospam@posteo.de>
2021-02-20 18:43:31 +0100

    Add product and customer to main class
    [Product]

--- src/main/java/com/tutorial/Main.java
+++ src/main/java/com/tutorial/Main.java
@@ -1,11 +1,17 @@
 package com.tutorial;
 
+import com.tutorial.module.Customer;
+import com.tutorial.module.Product;
+
 /**
  * @author maximilian lamm brain.free.kontakt@gmail.com
  * @project CoustomerOrderTutorial
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Product cola = new Product("Coca Cola 1.5l", 12.12, 1);
+        System.out.println(cola.get_price());
+        Customer greiner = new Customer("Marcus", "Greiner", 1);
+        System.out.println(greiner.get_id());
     }
 }
